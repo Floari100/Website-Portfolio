44e4b1866f09133756e6c0265ef76427ff7b5fe9
florian <F.Rexhaj.inf23@stud.bbbaden.ch>
2025-08-17 18:40:07 +0200

    chore: set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // wichtig: dein Repo-Name inkl. Slashes
   base: '/Website-Portfolio/',
-  build: { sourcemap: false } // optional, vermeidet .map-Themen
+  build: { sourcemap: false }
 })
