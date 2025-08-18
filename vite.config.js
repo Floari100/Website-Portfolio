1e87fb8111e39ce953178ac611f505d743a40096
florian <F.Rexhaj.inf23@stud.bbbaden.ch>
2025-08-18 07:52:14 +0200

    pages: use project base path and remove custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Wichtig: base = '/<Repo-Name>/'
 export default defineConfig({
   plugins: [react()],
   base: '/Website-Portfolio/',
